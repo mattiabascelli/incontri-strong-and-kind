d20ca489dee4b179a8f90569dd7332df66329c45
mattiabascelli <166599366+mattiabascelli@users.noreply.github.com>
2024-05-06 20:21:54 +0200

    Add files via upload

--- generatore.js
+++ generatore.js
@@ -13,7 +13,7 @@ function fightGenerator() {
             forzaAgilita.innerHTML = "FORZA +1 - AGILITA' +1";
             istintoCotenna.innerHTML = "ISTINTO -1 COTENNA 0";
             arma.innerHTML = "Bastone Consacrato - 1d6+1";
-            story.innerHTML = "Emidio è un pingue e amichevole rospo santone devoto a Santa Irene. Interessato al pettegolezzo e al sollazzo in parti uguali, ne sa una più del diavolo anche perché, se no, che santone sarebbe. Parla anche con le pietre della strada, non lascia niente di intentato, è la memoria storica del suo gruppo. Ricorda più le ricette delle preghiere e produce birra artigianale. Ha un animo ambiguo, vuole sapere sempre tutto ma di lui si sa poco, solo quello che lascia trasparire tra una portata e l'altra.";
+            story.innerHTML = "Emidio \u00E8 un pingue e amichevole rospo santone devoto a Santa Irene. Interessato al pettegolezzo e al sollazzo in parti uguali, ne sa una pi\u00F9 del diavolo anche perch\u00E8, se no, che santone sarebbe. Parla anche con le pietre della strada, non lascia niente di intentato, \u00E8 la memoria storica del suo gruppo. Ricorda pi\u00F9 le ricette delle preghiere e produce birra artigianale. Ha un animo ambiguo, vuole sapere sempre tutto ma di lui si sa poco, solo quello che lascia trasparire tra una portata e l'altra.";
             author.innerHTML = "Alain D'Ettorre";
             break;
         case 2:
@@ -26,7 +26,7 @@ function fightGenerator() {
             forzaAgilita.innerHTML = "FORZA +0 - AGILITA' +2";
             istintoCotenna.innerHTML = "ISTINTO +0 COTENNA +2";
             arma.innerHTML = "Arco Lungo Bucchianichese - 1d6+2";
-            story.innerHTML = "L'orso BEARnando è una versione anomala rispetto ai classici Orsi Marsicani: è piú magro degli altri (sará solo una questione di pelo?), ma comunque atletico ed inesauribile. Ha sempre uno zanio in spalla in quanto viaggiatore in terre lontane e vicine. Con o senza armatura, veste sempre con camicia dalle maniche arrotolate, bretelle, pantaloni corti e calzari che ricordano quasi delle Converse!";
+            story.innerHTML = "L'orso BEARnando \u00E8 una versione anomala rispetto ai classici Orsi Marsicani: \u00E8 piú magro degli altri (sará solo una questione di pelo?), ma comunque atletico ed inesauribile. Ha sempre uno zanio in spalla in quanto viaggiatore in terre lontane e vicine. Con o senza armatura, veste sempre con camicia dalle maniche arrotolate, bretelle, pantaloni corti e calzari che ricordano quasi delle Converse!";
             author.innerHTML = "Simone Zuccarini";
             break;
         case 3:
@@ -39,7 +39,7 @@ function fightGenerator() {
             forzaAgilita.innerHTML = "FORZA +2 - AGILITA' -1";
             istintoCotenna.innerHTML = "ISTINTO -1 COTENNA+2";
             arma.innerHTML = "Mattarello 'Judizie' - 1d4+2";
-            story.innerHTML = "Giannina è un’orsa anziana, vedova di lupo di mare che ogni tanto “scuoteva” per convincerlo di avere ragione. Dalla scomparsa del Compagno ha capito che la vita và vissuta bene, e per questo ha iniziato a fumare cicoria e giocare a carte con chiunque le capiti a tiro. Per Giannnina il cibo è sacro, anche se cade dal tavolo “basta soffiare”, e va’ mangiato tutto in un sol boccone. Sempre! Si narra che è impossibile mangiare lo stufato velocemente come lei, alcuni giurano di averla vista rincorrere un istrice per cibarsene.";
+            story.innerHTML = "Giannina \u00E8 un’orsa anziana, vedova di lupo di mare che ogni tanto scuoteva per convincerlo di avere ragione. Dalla scomparsa del Compagno ha capito che la vita va' vissuta bene, e per questo ha iniziato a fumare cicoria e giocare a carte con chiunque le capiti a tiro. Per Giannnina il cibo è sacro, anche se cade dal tavolo basta soffiare, e va’ mangiato tutto in un sol boccone. Sempre! Si narra che è impossibile mangiare lo stufato velocemente come lei, alcuni giurano di averla vista rincorrere un istrice per cibarsene.";
             author.innerHTML = "Moreno Ranalli";
             break;
         default:
@@ -52,7 +52,7 @@ function fightGenerator() {
             forzaAgilita.innerHTML = "FORZA +3 - AGILITA' +0";
             istintoCotenna.innerHTML = "ISTINTO -1 COTENNA+2";
             arma.innerHTML = "Ascia 'gnorante - 1d8+3";
-            story.innerHTML = "Isidoro è solo. Isidoro è in cerca di avventure, in cerca di uno scopo più grande. Sopravvive, lotta sempre. Puoi assumerlo come guardia del corpo o assoldarlo. Non fa domande, si getta nella mischia, CARICA... Porta a termine il suo compito. Ogni volta una nuova cicatrice, medaglia al valore sulla sua cotenna. Combatte, mangia e ama con pari foga. Il destino lo condurrà allo scopo grande. Nel frattempo si carica e BASTHA!";
+            story.innerHTML = "Isidoro \u00E8 solo. Isidoro \u00E8 in cerca di avventure, in cerca di uno scopo pi\u00F9 grande. Sopravvive, lotta sempre. Puoi assumerlo come guardia del corpo o assoldarlo. Non fa domande, si getta nella mischia, CARICA... Porta a termine il suo compito. Ogni volta una nuova cicatrice, medaglia al valore sulla sua cotenna. Combatte, mangia e ama con pari foga. Il destino lo condurr\u00E0 allo scopo grande. Nel frattempo si carica e BASTHA!";
             author.innerHTML = "Francesco Bucciarelli";
     }
 }
